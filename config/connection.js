2e3b23bc919aa691079fb3ffdc4a443de333c09e
Mary Alvarez <maria.alvarez9409@gmail.com>
2019-10-09 23:29:28 -0500

    deploy in mongo atlas

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const DB_PATH = require('./environment');
 
-mongoose.connect('mongodb://localhost:27017/lomito', {
+// mongodb://localhost:27017/lomito
+mongoose.connect('mongodb+srv://malvarez:201105@cluster0-azxjy.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
