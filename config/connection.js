3580408bc47d9b27a3d898bb8a5c6405b44cd699
Mary Alvarez <maria.alvarez9409@gmail.com>
2019-10-10 00:08:54 -0500

    adding mLab connection

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@ const DB_PATH = require('./environment');
 // mongodb://localhost:27017/lomito
 // mongodb+srv://malvarez:201105@cluster0-azxjy.mongodb.net/test?retryWrites=true&w=majority
 // mongodb://malvarez:201105mm@ds050739.mlab.com:50739/lomito 
+
 mongoose.connect('mongodb://malvarez:201105mm@ds050739.mlab.com:50739/lomito', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
