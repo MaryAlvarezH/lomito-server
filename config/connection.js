305253e0897fc7530c776646c99f47fc6a2dc235
Mary Alvarez <maria.alvarez9409@gmail.com>
2019-10-10 00:12:53 -0500

    mLab connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const DB_PATH = require('./environment');
 
-// mongodb://localhost:27017/lomito
-// mongodb+srv://malvarez:201105@cluster0-azxjy.mongodb.net/test?retryWrites=true&w=majority
-// mongodb://malvarez:201105mm@ds050739.mlab.com:50739/lomito 
+// local -> mongodb://localhost:27017/lomito
+// atlas -> mongodb+srv://malvarez:201105@cluster0-azxjy.mongodb.net/test?retryWrites=true&w=majority
+// mLab -> mongodb://malvarez:201105mm@ds050739.mlab.com:50739/lomito 
 
 mongoose.connect('mongodb://malvarez:201105mm@ds050739.mlab.com:50739/lomito', {
     useNewUrlParser: true,
