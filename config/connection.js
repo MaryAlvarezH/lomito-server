3fca5c57f4dbddfc56cb4e79bee9aa4332443ec3
Mary Alvarez <maria.alvarez9409@gmail.com>
2019-10-10 00:25:05 -0500

    adding mongo atlas connection

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const DB_PATH = require('./environment');
 // atlas -> mongodb+srv://malvarez:201105@cluster0-azxjy.mongodb.net/test?retryWrites=true&w=majority
 // mLab -> mongodb://malvarez:201105mm@ds050739.mlab.com:50739/lomito 
 
-mongoose.connect('mongodb://malvarez:201105mm@ds050739.mlab.com:50739/lomito', {
+mongoose.connect('mongodb+srv://malvarez:201105@cluster0-azxjy.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
